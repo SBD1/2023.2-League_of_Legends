dae2a11f5ce24727f2420e6aa73ee921ccc60666
brunotarquinio <107733709+brunotarquinio@users.noreply.github.com>
2023-10-30 17:15:09 -0300

    Corrige detalhes de DDL.sql

--- Módulo 2/DDL.sql
+++ Módulo 2/DDL.sql
@@ -1,31 +1,13 @@
--- Cria as tabelas do projeto
-
-CREATE TABLE Inferior (
-	nome_area char(8) PRIMARY KEY,
-	id_sala int,
-	tamanho int NOT NULL,
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
+CREATE TABLE Mapa (
+	Nome varchar(35) PRIMARY KEY,
+	Tamanho int NOT NULL
 );
 
-CREATE TABLE Meio (
-	nome_area char(4) PRIMARY KEY,
-	id_sala int, 
-	tamanho int NOT NULL,
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
-);
-
-CREATE TABLE Topo (
-	nome_area char(4) PRIMARY KEY,
-	id_sala int, 
-	tamanho int NOT NULL,
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
-);
-
-CREATE TABLE Spawn (
-	nome_area char(5) PRIMARY KEY,
-	id_sala int, 
-	tamanho int NOT NULL,
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
+CREATE TABLE Sala (
+	Id_sala int PRIMARY KEY
+	nome_mapa varchar (35),
+	Regiao varchar(30) NOT NULL,
+	FOREIGN KEY (nome_mapa) REFERENCES Mapa (Nome)
 );
 
 CREATE TABLE Personagem (
@@ -38,6 +20,7 @@ CREATE TABLE Personagem (
 	energia int NOT NULL,
 	dano int NOT NULL,
 	forca int NOT NULL,
+	cura int NOT NULL,
 	historia varchar(1000)
 );
 
@@ -57,59 +40,31 @@ CREATE TABLE NPC (
 	FOREIGN KEY (id_npc) REFERENCES Personagem (id_personagem) 
 );
 
-
-
-CREATE TABLE Jungle (
-	nome_area char(6) PRIMARY KEY,
+CREATE TABLE Loja (
+	nome_loja varchar(35) PRIMARY KEY,
+	local varchar(15) NOT NULL,
 	id_sala int, 
-	id_boss int,
-	tamanho int NOT NULL,
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala),
-	FOREIGN KEY (id_boss) REFERENCES Personagem (id_personagem)
-	 
-);
-
-CREATE TABLE Area_de_Combate (
-	nome_area_inferior char(8),
-	nome_area_meio char(4),
-	nome_area_topo char(4),
-	id_sala int, 
-	tamanho int NOT NULL,
-	FOREIGN KEY (nome_area_inferior) REFERENCES Inferior (nome_area),  
-	FOREIGN KEY (nome_area_meio) REFERENCES Meio (nome_area), 
-	FOREIGN KEY (nome_area_topo) REFERENCES Topo (nome_area),   
+	qtd_itens int NOT NULL,
 	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
 );
 
 CREATE TABLE Area_de_Recuperacao (
-	nome_area_spawn char(5),
-	id_sala int, 
-	tamanho int NOT NULL,
-	cura int NOT NULL,
-	FOREIGN KEY (nome_area_spawn) REFERENCES Spawn (nome_area),
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
+	nome_mapa varchar(35),
+	id_sala int,
+	recuperacao_vida_por_seg int NOT NULL,
+	recuperacao_energia_por_seg int NOT NULL
 );
-
-CREATE TABLE Loja (
-	nome_loja varchar(35) PRIMARY KEY,
-	nome_area char(5),
-	id_sala int, 
-	qtd_itens int NOT NULL,
-	FOREIGN KEY (nome_area) REFERENCES Spawn (nome_area),
-	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala) 
-);
+	
 
 CREATE TABLE Item (
 	id_item int PRIMARY KEY,
-	nome_item varchar(35),
+	nome_item varchar(35) NOT NULL,
+	local varchar(15) NOT NULL,
 	id_sala int, 
-	nome_area_spawn char(5),
-	nome_area_jungle char(6),
 	preco_item int NOT NULL,
 	dano_item int NOT NULL,	
 	FOREIGN KEY (id_sala) REFERENCES Sala (id_sala),
-	FOREIGN KEY (nome_area_spawn) REFERENCES Spawn (nome_area),
-	FOREIGN KEY (nome_area_jungle) REFERENCES Jungle (nome_area)
+
 	
 );
 
@@ -131,6 +86,7 @@ CREATE TABLE Instancia (
 CREATE TABLE Boss (
 	id_boss int,
 	recompensa varchar(35) NOT NULL,
+	local varchar(15) NOT NULL,
 	FOREIGN KEY (id_boss) REFERENCES Personagem (id_personagem)
 );
 
@@ -150,4 +106,11 @@ CREATE TABLE Itens_de_lutador (
 	id_item int,
 	nome_item varchar(25) NOT NULL,
 	dano_fisico_adicional int NOT NULL
+);
+
+CREATE TABLE Missoes (
+	id_missao int PRIMARY KEY,
+	id_npc int,
+	descricao varchar(100) NOT NULL,
+	nivel_necessario int NOT NULL
 );
